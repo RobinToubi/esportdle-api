01b537e17b188f7946165959eaa5d6f69b1fce7e
Robin Gimenez <gimenezrobin@protonmail.com>
2023-08-02 13:22:09 +0200

    feat : add older + team in result + optimize country initialization

--- comparer.go
+++ comparer.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/RobinToubi/countries"
+import (
+	"github.com/RobinToubi/countries"
+	"time"
+)
+
+var countryService *countries.CountryData
+
+func init() {
+	countryService = countries.InstanceCountry()
+}
 
 func Compare(guessedPlayer, currentResult Player) CompareResult {
 	return CompareResult{
@@ -8,6 +17,8 @@ func Compare(guessedPlayer, currentResult Player) CompareResult {
 		Role:    guessedPlayer.Role == currentResult.Role,
 		Country: IsCountryValidated(guessedPlayer.Country, currentResult.Country),
 		League:  guessedPlayer.League == currentResult.League,
+		Team:    guessedPlayer.Team == currentResult.Team,
+		Older:   isCurrentResultOlder(currentResult.BirthDate, guessedPlayer.BirthDate),
 	}
 }
 
@@ -15,7 +26,6 @@ func IsCountryValidated(guessedCountry, currentCountry string) string {
 	if guessedCountry == currentCountry {
 		return "VALID"
 	}
-	countryService := countries.InstanceCountry()
 	guessedContinent, err := countryService.GetContinentByCountry(guessedCountry)
 	if err != nil {
 		return ""
@@ -28,4 +38,10 @@ func IsCountryValidated(guessedCountry, currentCountry string) string {
 		return "NEAR"
 	}
 	return "FAR"
+}
+
+func isCurrentResultOlder(currentPlayerDate, guessedPlayerDate string) int {
+	currentPlayerTime, _ := time.Parse(time.RFC3339, currentPlayerDate)
+	guessedPlayerTime, _ := time.Parse(time.RFC3339, guessedPlayerDate)
+	return currentPlayerTime.Compare(guessedPlayerTime)
 }
--- guess_answer.go
+++ guess_answer.go
@@ -5,4 +5,12 @@ type CompareResult struct {
 	Role    bool   `json:"role"`
 	Country string `json:"country"`
 	League  bool   `json:"league"`
+	Team    bool   `json:"team"`
+	Older   int    `json:"older"`
+}
+
+type GuessResult struct {
+	Id          int           `json:"id"`
+	Player      Player        `json:"player"`
+	CompareData CompareResult `json:"compare"`
 }
