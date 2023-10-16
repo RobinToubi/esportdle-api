32a84bd4f80e73262af4347643b57eec6d7e87a4
Robin <gimenez.robin11@gmail.com>
2023-10-16 14:22:12 +0200

    config

--- server.go
+++ server.go
@@ -2,23 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalln("Error when loading .env file")
-	}
 	server := echo.New()
 	server.Use(middleware.CORSWithConfig(middleware.DefaultCORSConfig))
 	server.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello world.")
 	})
 	server.POST("/:playerId/guess", GuessPlayer)
-	server.Logger.Fatal(server.Start(fmt.Sprintf(":%s", "8080")))
+	server.Logger.Fatal(server.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
 }
