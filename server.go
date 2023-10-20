72f954b5e70a49fbad61b6b7c248fab12b9df4ed
Toubi <gimenez.robin11@gmail.com>
2023-10-20 09:24:17 +0200

    env: load local env variables

--- server.go
+++ server.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	_ = godotenv.Load()
 	server := echo.New()
 	server.Use(middleware.CORSWithConfig(middleware.DefaultCORSConfig))
 	server.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello world.")
 	})
 	server.POST("/:playerId/guess", GuessPlayer)
-	server.Logger.Fatal(server.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
+	server.Logger.Fatal(server.Start(fmt.Sprintf(":%s", "12000")))
 }
